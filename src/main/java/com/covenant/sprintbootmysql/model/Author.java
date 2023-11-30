cb14eddaa5b103274b52a5370d0f19506062a322
J1W0N-1209 <nabook754@naver.com>
2023-11-30 17:40:47 +0900

    ADD :: Author model

---
+++ src/main/java/com/covenant/sprintbootmysql/model/Author.java
@@ -0,0 +1,17 @@
+package com.covenant.sprintbootmysql.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "author")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
