80e3561dbea27474b2704114bc9c3529732e1950
J1W0N-1209 <nabook754@naver.com>
2023-11-30 12:41:50 +0900

    config db

---
+++ src/main/java/com/covenant/sprintbootmysql/SprintBootMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.covenant.sprintbootmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covenant/sprintbootmysql/SprintBootMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covenant.sprintbootmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
